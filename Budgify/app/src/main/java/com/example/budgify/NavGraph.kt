552d086be7d3fd45a371f09c5cbca00d995a3edb
Alessandro Rocchi <102317395+TheXbomber@users.noreply.github.com>
2025-05-06 14:24:18 +0200

    Fixed merged files

--- Budgify/app/src/main/java/com/example/budgify/NavGraph.kt
+++ Budgify/app/src/main/java/com/example/budgify/NavGraph.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.budgify.screen.Homepage
 import com.example.budgify.screen.ObjectivesManagementScreen
 import com.example.budgify.screen.ObjectivesScreen
+import com.example.budgify.screen.Settings
 
 @Composable
 fun NavGraph() {
@@ -18,5 +19,15 @@ fun NavGraph() {
         composable("home_screen") { Homepage(navController) }
         composable("objectives_screen") { ObjectivesScreen(navController) }
         composable("objectives_management_screen") { ObjectivesManagementScreen(navController) }
+        composable("settings_screen") { Settings(navController) }
+        /*
+        composable("manage_objectives_screen") { ManageObjectives(navController) }
+        composable("transactions_screen") { Transactions() }
+        composable("cred_deb_screen") { CredDeb() }
+        composable("credits_screen") { Credits() }
+        composable("debits_screen") { Debits() }
+        composable("categories_screen") { Categories() }
+        composable("adding_screen") { Adding() }
+         */
     }
 }
